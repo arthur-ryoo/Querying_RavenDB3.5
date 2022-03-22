10ed9e5e1781fb91018b64ee9bc8f75cc3d3a719
Arthur Ryoo <jene0523@gmail.com>
2022-03-22 11:59:10 -0700

    Add project files.

---
+++ DocumentStoreHolder.cs
@@ -0,0 +1,27 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryingRavenDB3._5
+{
+    public static class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                var store = new DocumentStore
+                {
+                    ConnectionStringName = "RavenDB"
+                };
+
+                return store.Initialize();
+            });
+
+        public static IDocumentStore Store =>
+            LazyStore.Value;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using QueryingRavenDB3._5.Models;
+using Raven.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryingRavenDB3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please, enter a company id (0 to exit): ");
+
+                int companyId;
+
+                if (!int.TryParse(Console.ReadLine(), out companyId))
+                {
+                    Console.WriteLine("Order # is invalid. ");
+                    continue;
+                }
+
+                if (companyId == 0) break;
+
+                QueryCompanyOrders(companyId);
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+
+        private static void QueryCompanyOrders(int companyId)
+        {
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                var orders = (
+                    from order in session.Query<Order>()
+                                            .Include(o => o.Company)
+                    where order.Company == $"companies/{companyId}"
+                    select order
+                    ).ToList();
+
+                var company = session.Load<Company>(companyId);
+
+                if (company == null)
+                {
+                    Console.WriteLine("Company not found");
+
+                    return;
+                }
+
+                Console.WriteLine($"Orders for {company.Name}");
+
+                foreach (var order in orders)
+                {
+                    Console.WriteLine($"{order.Id} - {order.OrderedAt}");
+                }
+            }
+        }
+    }
+}
